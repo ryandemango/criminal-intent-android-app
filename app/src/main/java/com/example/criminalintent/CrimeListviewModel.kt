c57ad24d3aa7adf0c686812c0f65e0d0c87c4141
ryandemango <notrmango@gmail.com>
2022-09-25 17:21:36 -0700

    Initial commit

--- app/src/main/java/com/example/criminalintent/CrimeListviewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListviewModel.kt
@@ -1,5 +1,19 @@
 package com.example.criminalintent
+import com.example.criminalintent.Crime
 
-class CrimeListviewModel {
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListviewModel : ViewModel() {
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 0 until 100) {
+            val crime = Crime()
+            crime.title = "Crime #$i"
+            crime.isSolved = i % 2 == 0
+            crimes += crime
+        }
+    }
 
 }
